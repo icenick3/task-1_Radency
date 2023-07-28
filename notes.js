f69c47c4d4b04e475a6df353559c341a02c9e5fc
kolya <kolyapadda@gmailc.com>
2023-07-28 14:46:56 +0300

    second commit

--- notes.js
+++ notes.js
@@ -1,8 +1,15 @@
 
 let notes = [
-    { id: 1, content: "I'm gonna have a dentist appointment on the 3/5/2021, I moved it from 5/5/2021", category: "Task", archived: false, dates: "3/5/2021", allDates: "3/5/2021" },
-  
+    { id: 1, name: "Dentist Appointment", content: "I'm gonna have a dentist appointment on the 3/5/2021, I moved it from 5/5/2021", category: "Task", archived: false, dates: "3/5/2021", allDates: "3/5/2021" },
+    { id: 2, name: "Buy Groceries", content: "Buy groceries for the weekend", category: "Task", archived: false, dates: "", allDates: "" },
+    { id: 3, name: "Mom's Birthday", content: "Call mom and wish her happy birthday 15/8/2021", category: "Task", archived: false, dates: "15/8/2021", allDates: "15/8/2021" },
+    { id: 4, name: "Prepare Presentation", content: "Prepare presentation slides for the meeting", category: "Task", archived: false, dates: "", allDates: "" },
+    { id: 5, name: "Meeting with Client", content: "Meeting with the client at 2 PM 20/7/2023", category: "Task", archived: false, dates: "20/7/2023", allDates: "20/7/2023" },
+    { id: 6, name: "Book Flight Tickets", content: "Book flight tickets for vacation", category: "Task", archived: false, dates: "", allDates: "" },
+    { id: 7, name: "Pay Electricity Bill", content: "Pay electricity bill", category: "Task", archived: false, dates: "", allDates: "" },
 ];
+
+
 
 function getNotes() {
     return notes;
